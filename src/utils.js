0b1acbec88d235c6ad7b366c3077ef27a0c94a77
Eddy <liang.741@buckeyemail.osu.edu>
2023-03-25 22:28:59 -0700

    url change

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const FLASK_BACKEND_URL = "http://172.31.4.102:5000/openai";
+const FLASK_BACKEND_URL = "http://localhost:5000/openai";
 
 export const getResponse = async (messages) => {
   const response = await axios.post(FLASK_BACKEND_URL, {
