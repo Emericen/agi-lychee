8157151c3da1ee45f93ea2c0c6dd6237ba73b0ef
Eddy <liang.741@buckeyemail.osu.edu>
2023-03-26 21:40:55 -0700

    updated url for dual server

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const FLASK_BACKEND_URL = "/openai";
+const FLASK_BACKEND_URL = "http://18.116.50.27/openai";
 
 export const getResponse = async (messages) => {
   const response = await axios.post(FLASK_BACKEND_URL, {
