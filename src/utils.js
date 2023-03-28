fa0cd0379bd5dd44dffcd93b7e646576ea44cc8a
Eddy <liang.741@buckeyemail.osu.edu>
2023-03-27 17:25:02 -0700

    added backend API key

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,22 @@
 import axios from "axios";
-
 const FLASK_BACKEND_URL = "http://18.116.50.27/openai";
+const BACKEND_API_KEY = process.env.REACT_APP_BACKEND_API_KEY;
 
 export const getResponse = async (messages) => {
-  const response = await axios.post(FLASK_BACKEND_URL, {
-    messages: messages,
-  });
+  console.log(BACKEND_API_KEY);
+  const headers = {
+    Authorization: BACKEND_API_KEY,
+  };
+
+  const response = await axios.post(
+    FLASK_BACKEND_URL,
+    {
+      messages: messages,
+    },
+    { headers }
+  );
 
   console.log(response.data);
 
   return response.data["content"];
-}
+};
